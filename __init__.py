b0fc70a7f6a4c9d6ba9a9f62b8c85e6d50c1c10c
Anand <2024aa05086@wilp.bits-pilani.ac.in>
2026-01-05 22:59:45 +0530

    test3

--- __init__.py
+++ __init__.py
@@ -1,1 +1,9 @@
+"""
+heart_disease_mlops package.
+
+This file makes 'heart_disease_mlops' a Python package so imports like:
+  python -m heart_disease_mlops.src.train
+work in CI and locally.
+"""
+__all__ = []
 """Heart Disease MLOps project package."""
